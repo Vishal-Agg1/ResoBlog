88b6c50162e40b97bc92226b009d71dbe883945b
vishal <vishalaggarwal4469359@gmail.com>
2024-10-13 22:02:07 +0530

    error resolve

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,13 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/container"; 
 import LogoutBtn from "./Header/LogoutBut";
+import Logo from "./Logo"
+import authSlice from '../store/authSlice'
 export {
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Logo,
+    authSlice
 };
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,22 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit"
-const initialstate = {
-    status: false,
-    userdata: null
-};
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status : false,
+    userData: null
+}
 
 const authSlice = createSlice({
     name: "User",
-    initialstate,
+    initialState,
     reducers: {
-      login:(state,action)=>{
-            state.status=true;
-            state.userdata=action.payload.userdata;
-      },
-      logout:(state)=>{
-        state.status=false;
-        state.userdata=null;
-      }
-    }
-});
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+     }
+})
+
 export const {login, logout} = authSlice.actions;
+
 export default authSlice.reducer;
