520bf039aa480684284963c67f089947db065fff
Vishal-Agg1 <vishalaggarwal4469359@gmail.com>
2024-10-14 09:25:40 +0530

    minor update

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container"; 
 import LogoutBtn from "./Header/LogoutBut";
 import Logo from "./Logo"
-import authSlice from '../store/authSlice'
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo,
-    authSlice
+    Logo
 };
