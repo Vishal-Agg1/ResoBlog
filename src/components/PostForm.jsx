f37e56feceb68180c890397098234abfac9854d9
vishal <vishalaggarwal4469359@gmail.com>
2024-10-24 20:26:04 +0530

    working on postform

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -7,6 +7,7 @@ import Select from "./Select";
 import service from "../Appwrite/database/dataservice";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import service from "../Appwrite/database/dataservice";
 export default function PostForm({post}){
     const {register,handleSubmit,watch,getValues,control,setValue} = useForm({
         defaultValues: {
@@ -19,7 +20,35 @@ export default function PostForm({post}){
     const navigate = useNavigate();
     const userData = useSelector(state=>state.auth.userData)
 
-    
+    const submit = async(data)=>{
+        if(post){
+            const file =await data.image[0]?service.uploadfile(data.image[0]):null
+            if(file){
+                service.delfile(post.featuredimage)
+            }
+            const dbPost = await service.updatepost({...data,slug:post.$id,
+              featuredimage: file? file.$id :undefined})
+              if(dbPost){
+                navigate(`/post/${dbpost.$id}`)
+              }
+        }
+        else{
+            //immprove this functionaality
+            const file = await service.uploadfile(data.image[0]);
+            if(file){
+                const fileId = file.$id;
+                data.featuredimage
+                = fileId;
+             const dbpost= service.createpost({...data,userid:userData.$id});
+             if(dbpost){
+                navigate(`/post/${(await dbpost).$id}`);
+             }
+            }
+            
+        }
+    }
+
+    const slugTransform
     return(
         <></>
     )
